99a7cb0de047aedcb9193eb6e289f5f4fddcc288
Dehou <dehou37@gmail.com>
2025-12-04 15:52:16 +0800

    include seeding data

--- backend/src/prisma.ts
+++ backend/src/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client";
 import dotenv from "dotenv";
 dotenv.config();
 
-const prisma = new PrismaClient({
+const prisma = new PrismaClient({});
   datasources: {
     db: {
       url: process.env.DATABASE_URL
